4c0a4356157cb299f7ece47892709db42121e91b
Cherryboome <muhamidifarian@yahoo.com>
2020-01-10 10:27:48 +0200

    formfields

--- 05-forms/src/components/user.js
+++ 05-forms/src/components/user.js
@@ -1,18 +1,85 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
+import FormFields from '../widgets/Forms/formFields'
 
 class User extends Component {
-
-    state = {
-        
+  state = {
+    formData: {
+      name: {
+        element: 'input',
+        value: '',
+        label: true,
+        labelText: 'Name',
+        config: {
+          name: 'name_input',
+          type: 'text',
+          placeholder: 'Enter your name'
+        }
+      },
+      lastname: {
+        element: 'input',
+        value: '',
+        label: true,
+        labelText: 'Lastname',
+        config: {
+          name: 'lastname_input',
+          type: 'text',
+          placeholder: 'Enter your lastname'
+        }
+      },
+      message: {
+        element: 'textarea',
+        value: '',
+        label: true,
+        labelText: 'Message',
+        config: {
+          name: 'message_input',
+          rows: 4,
+          cols: 36
+        }
+      },
+      age: {
+        element: 'select',
+        value: '',
+        label: true,
+        labelText: 'Select age',
+        config: {
+          name: 'age_input',
+          options: [
+            { val: '1', text: '10-20' },
+            { val: '2', text: '20-30' },
+            { val: '3', text: '30+' }
+          ]
+        }
+      }
     }
+  }
 
-    render(){
-        return(
-            <div>
-                User
-            </div>
-        )
+  updateForm = newState => {
+    this.setState({ formData: newState })
+  }
+
+  submitForm = event => {
+    event.preventDefault()
+    let dataToSubmit = {}
+
+    for (let key in this.state.formData) {
+      dataToSubmit[key] = this.state.formData[key].value
     }
+  }
+
+  render() {
+    return (
+      <div className='container'>
+        <form onSubmit={this.submitForm}>
+          <FormFields
+            formData={this.state.formData}
+            change={newState => this.updateForm(newState)}
+          />
+          <button type='submit'>Submit</button>
+        </form>
+      </div>
+    )
+  }
 }
 
-export default User;
+export default User
